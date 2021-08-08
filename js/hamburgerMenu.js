7cb2baee0befa9df0eab5f6b0f030d62ebdd7f93
Edgar Castaneda <67249405+Edgar-JC@users.noreply.github.com>
2021-08-08 07:32:15 -0500

    Barra lateral
    enlaces se movio a aside bar

--- js/hamburgerMenu.js
+++ js/hamburgerMenu.js
@@ -3,11 +3,13 @@ export const hamburgerMenu = () => {
   const hamburgerBtn = d.querySelector(".btn-hamburger");
   const panelEnlaces = d.querySelector(".enlaces");
   const main = d.querySelector(".main-pagina");
-  const loginInfo = d.querySelector(".login-info")
+
 
   hamburgerBtn.addEventListener("click", () => {
     panelEnlaces.classList.toggle("is-active");
     hamburgerBtn.classList.toggle("toggle");
     main.classList.toggle("oculto");
   });
+
+
 };
